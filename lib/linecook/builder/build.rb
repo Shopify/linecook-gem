a8242d38abd4e9a78467f2166ec3466d0d97d1e3
Dale Hamel <dale.hamel@shopify.com>
2015-12-20 15:46:23 -0500

    Bump and fix cleanup

--- lib/linecook/builder/build.rb
+++ lib/linecook/builder/build.rb
@@ -23,9 +23,14 @@ module Linecook
       @container.pause
       Linecook::Builder.ssh.run("sudo mksquashfs #{@container.root} #{path} -wildcards -e 'usr/src' 'var/lib/apt/lists/archive*' 'var/cache/apt/archives'") # FIXME make these excludes dynamic based on OS
       @container.resume
-      Linecook::Builder.ssh.download(path, local: File.join(Linecook::ImageManager::IMAGE_PATH, File.basename(path))) if save
-      Linecook::Builder.ssh.run("sudo rm -f #{path}")
-      path
+      path = if save
+        local_path = File.join(Linecook::ImageManager::IMAGE_PATH, File.basename(path))
+        Linecook::Builder.ssh.download(path, local: local_path)
+        Linecook::Builder.ssh.run("sudo rm -f #{path}")
+        local_path
+      else
+        path
+      end
     end
   end
 end
