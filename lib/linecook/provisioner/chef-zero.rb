46e3ab4eeeeb67fc92dc4803b7ee8c22d8d7fb49
Dale Hamel <dale.hamel@shopify.com>
2015-12-22 09:54:24 -0500

    Forcefully set hostname

--- lib/linecook/provisioner/chef-zero.rb
+++ lib/linecook/provisioner/chef-zero.rb
@@ -25,6 +25,7 @@ module Linecook
       build.ssh.forward(chef_port)
       build.ssh.upload(script, '/tmp/chef_bootstrap')
       build.ssh.run('[ -f /var/chef/cache/chef-client-running.pid ] && sudo rm -f /var/chef/cache/chef-client-running.pid || true')
+      build.ssh.run("sudo hostname #{chef_config[:node_name]}")
       build.ssh.run('sudo bash /tmp/chef_bootstrap')
       build.ssh.run('sudo rm -rf /etc/chef')
       build.ssh.stop_forwarding
