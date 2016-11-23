efeae76486915713990953d1414a2bf3576f9f0e
Dale Hamel <dale.hamel@shopify.com>
2016-11-23 15:51:33 -0500

    Fix

--- lib/linecook-gem/packager/packer.rb
+++ lib/linecook-gem/packager/packer.rb
@@ -60,7 +60,7 @@ module Linecook
       'DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y --force-yes --no-upgrade install grub-pc grub-legacy-ec2',
       'update-grub',
       'rm -f /etc/init/fake-container-events.conf', # HACK
-      'mkdir -p /run/resolvconf',
+      'mkdir -p /run/resolvconf/interface',
       'DEBIAN_FRONTEND=noninteractive dpkg-reconfigure resolvconf' # re-linkify resolvconf
     ]
 
